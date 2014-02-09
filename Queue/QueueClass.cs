cad939c30b2803babc4f90b427b02da605bad357
Oscarbralo <oscarbralo@outlook.com>
2014-02-09 10:49:07 +0100

    new data structures added

---
+++ Queue/QueueClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queue
+{
+    public class QueueClass
+    {
+        private Node first;
+        private Node tail;
+        private int numberOfNodes = 0;
+
+        public class Node
+        {
+            public string item;
+            public Node next;
+
+            public Node(string element)
+            {
+                item = element;
+                next = null;
+            }
+        }
+
+        public void Enqueue(string element)
+        {
+            Node newNode = new Node(element);
+            if (first == null)
+            {
+                first = newNode;
+                first.next = tail;
+                tail = first;
+            }
+            else
+            {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            numberOfNodes++;
+        }
+
+        public string Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                string item = first.item;
+                first = first.next;
+                numberOfNodes--;
+                return item;
+            }
+            else
+            {
+                return "No more elements in the queue";
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            return numberOfNodes == 0;
+        }
+
+        public int StillInQueue()
+        {
+            return numberOfNodes;
+        }
+    }
+}
