d68776c37807b5df442969acbefbc4411a5487d5
Oscarbralo <oscarbralo@outlook.com>
2014-02-09 14:28:40 +0100

    new data structures added

---
+++ QueueGeneric/QueueGeneric.cs
@@ -0,0 +1,61 @@
+﻿namespace Queue
+{
+    public class QueueGeneric<T>
+    {
+        T empty;
+        private Node<T> first;
+        private Node<T> tail;
+        private int numberOfNodes = 0;
+
+        public class Node<T>
+        {
+            public T item;
+            public Node<T> next;
+
+            public Node(T element)
+            {
+                item = element;
+                next = null;
+            }
+        }
+
+        public void Enqueue(T element)
+        {
+            Node<T> newNode = new Node<T>(element);
+            if (first == null)
+            {
+                first = newNode;
+                first.next = tail;
+                tail = first;
+            }
+            else
+            {
+                tail.next = newNode;
+                tail = newNode;
+            }
+            numberOfNodes++;
+        }
+
+        public T Dequeue()
+        {
+            if (!IsEmpty())
+            {
+                T item = first.item;
+                first = first.next;
+                numberOfNodes--;
+                return item;
+            }
+            return empty;
+        }
+
+        public bool IsEmpty()
+        {
+            return numberOfNodes == 0;
+        }
+
+        public int StillInQueue()
+        {
+            return numberOfNodes;
+        }
+    }
+}
